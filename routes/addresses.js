56ae0d34cfa40b254334315660c8ffb2b8231697
jalarg <jlema1990@gmail.com>
2023-05-30 18:49:01 -0300

    jwt y bcrypt para creacion de usuarios

--- routes/addresses.js
+++ routes/addresses.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { 
-getAll, 
-findOneUser,
-createOneUser } = require("../controllers/users");
+const { getAll } = require("../controllers/addresses");
 
 //-----------RUTAS GENERALES-------------//
 
 // OBTENER TODOS LOS USUARIOS CHECK PARA CREAR CUENTA
 router.get("/", getAll);
-
-// OBTENER UN USUARIO ESPECIFICO
-router.get("/:_id", findOneUser);
-
-// CREAR UN USUARIO
-router.post("/new", createOneUser);
 
 module.exports = router;
